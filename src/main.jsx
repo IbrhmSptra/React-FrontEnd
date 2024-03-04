202549471f810431d94a44436cb65a08ce86e1a9
immanuelbayumurti <154411494+immanuelbayumurti@users.noreply.github.com>
2024-03-04 18:03:39 +0800

    setup recipe page dulu

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage.jsx";
 import { Provider } from "react-redux";
 import Store from "./redux/store.js";
 import DetailTransaksi from "./pages/DetailTransaksi.jsx";
+import Recipepage from "./pages/Recipepage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,11 @@ const router = createBrowserRouter([
   {
     path: "/DetailTransaksi",
     element: <DetailTransaksi />,
+  },
+
+  {
+    path: "/Recipepage",
+    element: <Recipepage />,
   },
 ]);
 
---
+++ src/pages/Recipepage.jsx
@@ -0,0 +1,15 @@
+import Footer from "../layouts/Footer";
+import Navbar from "../layouts/Navbar";
+
+
+const Recipepage = () => {
+  return (
+    <>
+      <Navbar />
+   
+      <Footer />
+    </>
+  );
+};
+
+export default Recipepage;
