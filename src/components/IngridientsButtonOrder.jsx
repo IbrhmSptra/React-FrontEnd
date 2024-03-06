e359028a982d4b60b86a8d40d3e081f4c2958350
immanuelbayumurti <154411494+immanuelbayumurti@users.noreply.github.com>
2024-03-06 19:51:19 +0800

    nambahin star rating

--- src/components/IngridientsButtonOrder.jsx
+++ src/components/IngridientsButtonOrder.jsx
@@ -4,7 +4,7 @@ import iconcart from "../assets/img/cart-icon.png"
 const IngridientsButtonOrder = () => {
   return (
     <Link to="/DetailTransaksi">
-        <div className="inline-flex mt-4 justify-around p-2 w-1/2 bg-primary rounded-lg sm:mx-auto">
+        <div className="inline-flex mt-4 justify-around p-2 w-1/2 rounded-lg sm:mx-auto bg-primary">
         <img
           alt="shoppingcart"
           src={iconcart}
